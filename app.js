779dc1ab671c4f1c2fc7aff93dcd40ba5976d6e0
hsperez6 <118867950+hsperez6@users.noreply.github.com>
2023-11-08 09:53:33 -0800

    Post Create Quote

--- app.js
+++ app.js
@@ -1,38 +1,54 @@
 const express = require('express');
 const app = express();
 
+const records = require('./records');
+
+app.use(express.json());
+
 // Send a GET request to /quotes to READ a list of quotes
-app.get('/quotes', (req, res)=>{
-    res.json(data);
+app.get('/quotes', async (req, res)=>{
+  
+  try{
+    const quotes = await records.getQuotes();
+    res.json(quotes);
+  } catch(err) {
+    res.json({message: err.message})
+  }
+
 });
+
 // Send a GET request to /quotes/:id to READ(view) a quote
-app.get('/quotes/:id', (req, res)=>{
-    const quote = data.quotes.find(quote => quote.id == req.params.id);
+app.get('/quotes/:id', async (req, res)=>{
+
+  try{
+    const quote = await records.getQuote(req.params.id);
     res.json(quote);
+  } catch(err){
+    res.json({message: err.message})
+  }
+
 });
+
 // Send a POST request to /quotes to  CREATE a new quote 
+app.post('/quotes', async (req, res)=>{
+
+  try {    
+    const newQuote = await records.createQuote({
+      quote: req.body.quote,
+      author: req.body.author
+    });
+    res.json(newQuote);
+  } catch(err){
+    res.json({message: err.message})
+  }
+
+});
+
+
 // Send a PUT request to /quotes/:id to UPDATE (edit) a quote
+
 // Send a DELETE request to /quotes/:id DELETE a quote 
+
 // Send a GET request to /quotes/quote/random to READ (view) a random quote
 
 app.listen(3000, () => console.log('Quote API listening on port 3000!'));
-
-const data = {
-    quotes: [
-      {
-        id: 8721,
-        quote: "We must accept finite disappointment, but we must never lose infinite hope.",
-        author: "Martin Luther King"
-      },
-      {
-        id: 5779,
-        quote: "Use what you’ve been through as fuel, believe in yourself and be unstoppable!",
-        author: "Yvonne Pierre"
-      },
-      {
-        id: 3406,
-        quote: "To succeed, you have to do something and be very bad at it for a while. You have to look bad before you can look really good.",
-        author: "Barbara DeAngelis"
-      }
-    ]
-  }
